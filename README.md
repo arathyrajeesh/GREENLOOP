99166c388acb99500e223cf30debb467d3fef902
arathyrajeesh <arathyrajeesh2@gmail.com>
2026-03-25 10:00:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GREENLOOP
+
+A scalable, domain-driven Django 5.x architecture backend with split settings and ASGI Channels setup for GreenLoop.
---
+++ greenloop/asgi.py
@@ -0,0 +1,22 @@
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import greenloop.routing
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "greenloop.settings.development"
+)
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            greenloop.routing.websocket_urlpatterns
+        )
+    ),
+})
