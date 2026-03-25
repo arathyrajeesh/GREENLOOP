b81648366195754e455dcaea93a3a8b84fc06f97
arathyrajeesh <arathyrajeesh2@gmail.com>
2026-03-25 12:19:25 +0530

    feat: redirect root URL to API documentation

--- greenloop/urls.py
+++ greenloop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+from django.views.generic import RedirectView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -15,6 +16,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
     path('admin/', admin.site.urls),
     re_path(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
